86e74f6472cb59a6bc3fff8e47c389a809127d06
lainegates <lainegates@163.com>
2013-11-07 14:48:25 +0800

    link osg lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <osg/PositionAttitudeTransform>
+#include <osgdb/ReadFile>
+#include <osgViewer/Viewer>
+#include <osg/io_utils>
 using namespace std;
 int main()
 {
-	cout<<"hello world!"<<endl;
+    osg::Node* model = osgDB::readNodeFile("cow.osg");
+
+    osg::ref_ptr<osg::PositionAttitudeTransform> pat = new osg::PositionAttitudeTransform;
+    pat->addChild(model);
+
+    osgViewer::Viewer viewer;
+    viewer.setSceneData(pat.get());
+
+    return viewer.run();
+    cout<<"hello world!"<<endl;
 }
